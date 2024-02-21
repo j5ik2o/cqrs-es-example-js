b672ce1262667d790253e1718f56b90d524e5ad2
Junichi Kato <j5ik2o@gmail.com>
2024-02-21 16:30:26 +0900

    add impls

---
+++ packages/rmu/src/group-chat-dao.ts
@@ -0,0 +1,13 @@
+import {GroupChatId, GroupChatName, UserAccountId} from "cqrs-es-example-js-command-domain";
+
+interface GroupChatDao {
+    insertGroupChat: (aggregateId: GroupChatId, name: GroupChatName, administratorId: UserAccountId, createdAt: Date) => void;
+}
+
+const GroupChatDao = {
+}
+
+
+export { GroupChatDao };
+
+
