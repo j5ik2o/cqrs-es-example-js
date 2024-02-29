ced130633f1e27b3326da412a45cb61c1c97f70f
Junichi Kato <j5ik2o@gmail.com>
2024-02-29 11:28:09 +0900

    fix: add a script file

---
+++ tools/scripts/delete-all-node-modules.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+rm -fr node_modules
+rm -fr packages/bootstrap/node_modules
+rm -fr packages/command/domain/node_modules
+rm -fr packages/command/interface-adaptor-if/node_modules
+rm -fr packages/command/interface-adaptor-impl/node_modules
+rm -fr packages/command/use-case/node_modules
+rm -fr packages/query/interface-adaptor/node_modules
+rm -fr packages/rmu/node_modules
